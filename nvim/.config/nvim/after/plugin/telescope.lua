4b3af66c8f46682ff7d50a470c3360673cb1677c
bunnie <gbhagwaria@gmail.com>
2025-09-21 12:49:25 +0530

    tweaked nvim config

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,6 +2,14 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>h', builtin.help_tags)
+
+vim.keymap.set("n", "<leader>do", function()
+    builtin.find_files({
+        cwd = "~/dotfiles",
+        hidden = true,
+        find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" }
+    })
+end, { desc = "Find files in dotfiles directory" })
