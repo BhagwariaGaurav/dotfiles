1288b6da886cfadb2fcec587306ad168edc1158a
Gaurav <gbhagwaria@gmail.com>
2026-07-31 22:49:02 +0530

    added scripts for kbd handling

--- scripts/scripts/kb_post.sh
+++ scripts/scripts/kb_post.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-hyprctl eval 'hl.config({ input = { kb_layout = "us,us,ru", kb_variant = "colemak_dh,," } })'
+hyprctl eval 'hl.config({ input = { kb_layout = "us,us,ru", kb_variant = "colemak_dh,," } })' \
+  && notify-send "Hyprland" "Pre-launch keyboard config applied" \
+  || notify-send -u critical "Hyprland" "Pre-launch keyboard config FAILED"
