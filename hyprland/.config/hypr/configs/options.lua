093156ca16e263a9d1686e935632f1f9224fc111
Gaurav <gbhagwaria@gmail.com>
2026-08-11 00:03:54 +0530

    added a different cursor

--- hyprland/.config/hypr/configs/options.lua
+++ hyprland/.config/hypr/configs/options.lua
@@ -11,6 +11,7 @@ hl.config({
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "WhiteSur cursors")
 
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("invidious_instance","https://invidious.flokinet.to")
